cd0648c3e10266a8453fa43bb61e239930ccf861
Andrii Gerasymenko <30573650+Hatgor@users.noreply.github.com>
2025-12-14 17:37:24 +0100

    fix: use proper icons for app and system tray, add release dmg styles (#9)
    * chore: add app & tray icons
    
    * chore: use create-dmg to create styled release dmg

---
+++ dmg/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configuration Variables for McBridger DMG creation
+
+# DMG Appearance
+export DMG_VOLNAME="McBridger"
+export DMG_NAME="McBridger.dmg"
+export DMG_BACKGROUND_IMG="./dmg/background.png"
+export DMG_WINDOW_POS="300 120"
+export DMG_WINDOW_SIZE="500 325"
+export DMG_ICON_SIZE="80"
+export DMG_APP_ICON_POS="165 110"      # Coordinates for the app icon
+export DMG_APP_DROP_LINK_POS="335 109" # Coordinates for the /Applications folder link
